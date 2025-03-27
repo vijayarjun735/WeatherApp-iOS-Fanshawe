3dcb0e2e9a79f2ee05fbd937247d24917458cfb1
Arjun V <vijayarjun735@gmail.com>
2025-03-26 23:13:44 -0400

    initial commit
    project created

---
+++ FanshaweWeather/FanshaweWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FanshaweWeather
+//
+//  Created by Arjun V on 2025-03-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FanshaweWeather/FanshaweWeatherTests/FanshaweWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  FanshaweWeatherTests.swift
+//  FanshaweWeatherTests
+//
+//  Created by Arjun V on 2025-03-26.
+//
+
+import Testing
+@testable import FanshaweWeather
+
+struct FanshaweWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
